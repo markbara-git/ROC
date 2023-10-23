8eee171fe27c2badfb939ba50f8ed642cb7a85b1
markbara-git <jacekkuzyn87@gmail.com>
2023-10-23 11:09:07 +0200

    Added comments

--- roc_final.py
+++ roc_final.py
@@ -6,8 +6,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 
+#path to input csv
 input = "input\\input-we.csv"
 
+#get the related columns
 data = pd.read_csv(input,usecols=['geopixel', 
                                   'cve_css_total_score',
                                   'osi_model_layer1_security_posture_assessment',
@@ -15,14 +17,23 @@ data = pd.read_csv(input,usecols=['geopixel',
                                   'osi_model_layer3_security_posture_assessment',
                                   'os_os_security_assessment'])
 
+#prepare the 'cve_css_total_score'
 data['cve_css_total_score'] = data['cve_css_total_score'].values * 0.1
+
+#group data by 'goepixel'
 data = data.groupby('geopixel').mean().reset_index()
+
+#append the 'score' column based on 'cve_css_total_score
 data['score'] = data['cve_css_total_score'].apply(lambda x: 1 if x >= 0.5 else 0)
+
+#append the 'score2' column based on the 'os_os_security_assessment'
 data['score2'] = data['os_os_security_assessment'].apply(lambda x: 1 if x >= 0.5 else 0)
 #data.to_csv('input\\slim.csv',index=False)
 #print(data)
 
 #data.to_csv("input\\slim10.csv",index=False)
+
+
 X = data[['score2',
           'osi_model_layer1_security_posture_assessment',
           'osi_model_layer2_security_posture_assessment',
@@ -30,8 +41,10 @@ X = data[['score2',
           'os_os_security_assessment']]
 y = data['score']
 
+#train data
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3, random_state=0)
 
+#Logistic Regresion
 log_regression = LogisticRegression()
 log_regression.fit(X_train, y_train)
 
@@ -47,8 +60,6 @@ fpr, tpr, _ = roc_curve(y_test, y_pred_proba)
 y_pred_proba = log_regression.predict_proba(X_test)[::,1]
 fpr, tpr, _ = metrics.roc_curve(y_test,  y_pred_proba)
 auc = metrics.roc_auc_score(y_test, y_pred_proba)
-
-
 
 #create ROC curve
 plt.plot(fpr,tpr,label="AUC="+str(auc))
