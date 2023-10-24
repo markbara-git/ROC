3bd0d2f4e5d94f09175a3085d5f5cc7a9ef58ea3
markbara-git <jacekkuzyn87@gmail.com>
2023-10-24 09:45:43 +0200

    Code cleaning pt.1

--- roc_final.py
+++ roc_final.py
@@ -1,10 +1,8 @@
 import pandas as pd
-import numpy as np
 from sklearn.metrics import roc_curve, roc_auc_score, auc, confusion_matrix
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn import metrics
 
 #path to input csv
 input = "input\\input-we.csv"
@@ -58,8 +56,8 @@ fpr, tpr, _ = roc_curve(y_test, y_pred_proba)
 
 #define metrics
 y_pred_proba = log_regression.predict_proba(X_test)[::,1]
-fpr, tpr, _ = metrics.roc_curve(y_test,  y_pred_proba)
-auc = metrics.roc_auc_score(y_test, y_pred_proba)
+fpr, tpr, _ = roc_curve(y_test,  y_pred_proba)
+auc = roc_auc_score(y_test, y_pred_proba)
 
 #create ROC curve
 plt.plot(fpr,tpr,label="AUC="+str(auc))
