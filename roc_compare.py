40263b7cc2fc2c04bd5a9778bbbcf5099f03871b
markbara-git <fackalata@gmail.com>
2023-11-20 10:52:04 +0100

    update

--- roc_compare.py
+++ roc_compare.py
@@ -4,7 +4,11 @@ import pandas as pd
 input = "input\\input-0-nr.csv"
 output = "output\\"
 
+#read CSV
 df = pd.read_csv(input,usecols=['geopixel','device_total_security_score'])
+
+#group by geopixel and calculate the arithmetic mean of the device_total_security_score
 df = df.groupby(by='geopixel').mean().reset_index()
 
+#save data to CSV
 df.to_csv(output+"compare.csv",index=False)
