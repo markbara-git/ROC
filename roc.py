87516021d61f5ee32cf1d08260b6cb7f08cec3f1
markbara-git <fackalata@gmail.com>
2023-11-28 14:41:14 +0100

    update

--- roc.py
+++ roc.py
@@ -1,41 +1,94 @@
 import pandas as pd
-import numpy as np
-from sklearn.metrics import roc_curve, roc_auc_score
+import os
+from sklearn.metrics import roc_curve, roc_auc_score, auc, confusion_matrix
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
-input = "input\\input-we-10-slim.csv"
+#path to input csv
+input = "input\\input-0-nr.csv"
+output = "output\\"
 
-# Load CSV file into a pandas DataFrame
-df = pd.read_csv(input)
+#get the related columns
+data = pd.read_csv(input,usecols=['geopixel', 
+                                  'cve_css_total_score',
+                                  'osi_model_layer1_security_posture_assessment',
+                                  'osi_model_layer2_security_posture_assessment',
+                                  'osi_model_layer3_security_posture_assessment',
+                                  'os_os_security_assessment'])
 
-# Group DataFrame by unique geopixel
-groups = df.groupby('geopixel')
+data = data.fillna(0.5)
 
-# Initialize figure and axes
-fig, ax = plt.subplots()
+#prepare the 'cve_css_total_score'
+data['cve_css_total_score'] = data['cve_css_total_score'].values * 0.1
 
-# Iterate over groups and plot ROC curve for each column
-for name, group in groups:
-    # Extract required columns and convert to numpy arrays
-    y_true = group['geopixel'].values
-    y_score = group[['cve_css_total_score', 'os_os_security_assessment', 'osi_model_layer1_security_posture_assessment', 'osi_model_layer2_security_posture_assessment', 'osi_model_layer3_security_posture_assessment']].values
+#group data by 'goepixel'
+data = data.groupby('geopixel')
 
-    # Compute ROC curve and AUC score for each column
-    fpr = dict()
-    tpr = dict()
-    roc_auc = dict()
-    for i in range(y_score.shape[1]):
-        fpr[i], tpr[i], _ = roc_curve(y_true, y_score[:, i])
-        roc_auc[i] = roc_auc_score(y_true, y_score[:, i])
+#lists to store data
+geolist = []
+auclist = []
 
-    # Plot ROC curves for each column
-    ax.plot([0, 1], [0, 1], 'k--')
-    ax.set_xlim([0.0, 1.0])
-    ax.set_ylim([0.0, 1.05])
-    ax.set_xlabel('False Positive Rate')
-    ax.set_ylabel('True Positive Rate')
-    ax.set_title(f'ROC Curve for {name}')
-    for i in range(y_score.shape[1]):
-        ax.plot(fpr[i], tpr[i], label=f'{i} (AUC = {roc_auc[i]:.2f})')
-    ax.legend(loc="lower right")
-plt.show()
+#spliting by geopixel into seperate CSV files
+for name, group in data:
+    group.to_csv(output+f'{name}.csv',index=False)
+    geolist.append(output+f'{name}.csv')
+
+#proccesing each geopixel
+for geo in geolist:
+    data = pd.read_csv(geo)
+    try:
+        #append the 'score' column based on 'cve_css_total_score
+        data['score'] = data['cve_css_total_score'].apply(lambda x: 1 if x >= 0.5 else 0)
+
+        #append the 'score2' column based on the 'os_os_security_assessment'
+        data['score2'] = data['os_os_security_assessment'].apply(lambda x: 1 if x >= 0.5 else 0)
+        
+        X = data[['score2',
+                  'osi_model_layer1_security_posture_assessment',
+                  'osi_model_layer2_security_posture_assessment',
+                  'osi_model_layer3_security_posture_assessment',
+                  'os_os_security_assessment']]
+        y = data['score']
+        
+        #train data
+        X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3, random_state=0)
+
+        #Logistic Regresion
+        log_regression = LogisticRegression()
+        log_regression.fit(X_train, y_train)
+
+        y_pred = log_regression.predict(X)
+
+        #create confusion matrix
+        confusionMatrix = confusion_matrix(y,y_pred)
+        #define metrics
+        y_pred_proba = log_regression.predict_proba(X_test)[::,1]
+        fpr, tpr, _ = roc_curve(y_test,  y_pred_proba)
+
+        auc = roc_auc_score(y_test, y_pred_proba)
+        auclist.append(auc)        
+    except:
+        #if not enough data then set the AUC value to 0
+        auclist.append(0)
+    
+    #deleting temp csv    
+    os.remove(geo)
+
+#formating the geolist
+geolist = [s[:-4] for s in geolist]
+geolist = [s[7:] for s in geolist]
+
+#creating the CSV output
+df = pd.DataFrame({'geopixel': geolist, 'auc': auclist})
+df = df.sort_values(by='geopixel', ascending=True)
+df.to_csv(output+'output-nr.csv',index=False)
+
+
+#create ROC curve
+#plt.plot(fpr,tpr,label="AUC="+str(auc))
+#plt.ylabel('True Positive Rate')
+#plt.xlabel('False Positive Rate')
+#plt.legend(loc=4)
+#plt.show()
+
